dc88a7270f2c1c5a9367fa53d84a6ceb16c920a3
seTc1 <sema2234.archi@gmail.com>
2024-11-17 13:35:38 +0300

    [1.5] Database Open

--- SimpleSQliteReaderProject/MainWindow.py
+++ SimpleSQliteReaderProject/MainWindow.py
@@ -1,7 +1,7 @@
-from PyQt6.QtSql import QSqlDatabase
-
+from PyQt6.QtSql import QSqlDatabase, QSqlTableModel
+import sqlite3
 from MainWindowUI import Ui_MainWindow
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QTableWidgetItem
 from ExecuteSQLWindow import ExecuteSQLWindow
 
 
@@ -10,10 +10,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.btn_execute_sql.clicked.connect(self.open_sqlexecute_window)
+        self.buttons_connection()
         self.executeSQlWindow = ExecuteSQLWindow()
-        self.database = None
+        self.database_connection = None
         self.model = None
 
+    def buttons_connection(self):
+        self.btn_open_database.clicked.connect(self.open_database)
+        self.combobox_chose_window.currentIndexChanged.connect(self.load_table)
 
     def open_sqlexecute_window(self):
         self.executeSQlWindow.show()
@@ -27,16 +31,40 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.statusBar().showMessage("Ошибка, неверное расширение файла!")
             return
 
-        if self.database:
-            self.database.close()
+        if self.database_connection:
+            self.database_connection.close()
 
-        self.database = QSqlDatabase.addDatabase("QSQLITE")
-        self.database.setDatabaseName(database_name)
+        self.database_connection = sqlite3.connect(database_name)
 
-        if not self.database.open():
-            self.statusBar().showMessage("Ошибка открытия базы данных!")
+        self.load_table_names() 
+        self.load_table()
+        self.statusBar().showMessage(f"База данных {database_name} успешно открыта!")
+
+    def load_table_names(self):
+        cursor = self.database_connection.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = cursor.fetchall()
+
+        self.combobox_chose_window.clear()
+        for table in tables:
+            self.combobox_chose_window.addItem(table[0])
+
+    def load_table(self):
+        table_name = self.combobox_chose_window.currentText()
+        if not table_name:
             return
 
-        self.statusBar().showMessage(f"База данных {database_name} успешно открыта!")
-        #self.populate_table_combobox()
+        cursor = self.database_connection.cursor()
+        sql_query = f"SELECT * FROM {table_name}"
+        result = cursor.execute(sql_query).fetchall()
+
+        self.tableWidget_database_content.setRowCount(len(result))
+        self.tableWidget_database_content.setColumnCount(len(cursor.description))
+
+        headers = [description[0] for description in cursor.description]
+        self.tableWidget_database_content.setHorizontalHeaderLabels(headers)
+
+        for row_idx, row in enumerate(result):
+            for col_idx, value in enumerate(row):
+                self.tableWidget_database_content.setItem(row_idx, col_idx, QTableWidgetItem(str(value)))
 
